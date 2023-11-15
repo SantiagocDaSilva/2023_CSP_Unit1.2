2ddf5f6c54e8ec5b6755f7e60792e90607d4b342
SantiagocDaSilva <santiago.dasilva@vtsd.com>
2023-11-15 10:07:55 -0500

    challenge 11/15/23

---
+++ challenge.py
@@ -0,0 +1,23 @@
+asterisk = "*"
+asterisk2 = "*"
+
+'''print(asterisk)
+for a in range(4):
+    asterisk = asterisk + (asterisk2)
+    print(asterisk)
+'''
+def outcome_asterisk():
+    asterisk = "*"
+    asterisk2 = "*"
+    print(asterisk)
+    for a in range(4):
+        asterisk = asterisk + (asterisk2)
+        print(asterisk)
+
+
+outcome_asterisk()
+
+
+
+
+
