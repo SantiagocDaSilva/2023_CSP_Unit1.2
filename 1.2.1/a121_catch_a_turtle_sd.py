32104844b4bc963c43779da0d11bc78249b917f7
SantiagocDaSilva <santiago.dasilva@vtsd.com>
2023-11-29 07:58:58 -0500

    1.2.2 leaderboard 11/29/23

--- 1.2.1/a121_catch_a_turtle_sd.py
+++ 1.2.1/a121_catch_a_turtle_sd.py
@@ -1,84 +1,53 @@
-# a121_catch_a_turtle.py
 # -----import statements-----
 import turtle as trtl
-t = trtl.Turtle()
 import random as rand
-score_writer = trtl.Turtle()
 import leaderboard as lb
-counter = trtl.Turtle()
-
-# -----game configuration----
-
-score = 0
-t_color = ["red", "orange", "yellow", "green", "cyan", "blue", "purple", "silver"]
-t_size = [0.5, 0.75, 1, 1.5, 2, 2.5, 3]
-bg_color = ["green", "blue", "purple"]
-t_shape = "circle"
+t=trtl.Turtle()
+score_writer = trtl.Turtle()
+# -----game configuration-----
+# To view in trinket change the values of font_size, spot_size, and
+# screen_size by half
+colors = ["black", "sky blue", "salmon", "orchid", "pale green"]
 font_setup = ("Arial", 20, "normal")
+t_size = 2
+t_color = 'pink'
+t_shape = "turtle"
+
+# Changed from 30 to 5 for testing purposes
+#timer = 30
 timer = 5
+
 counter_interval = 1000
 timer_up = False
+score = 0
+leaderboard_file_name = "a122_leaderboard.txt"
+player_name = input("What is your name?")
 
-# -----initialize turtle-----
-leaderboard_file_name = "leaderboard.py"
-leader_names_list = []
-leader_scores_list = []
-player_name = input("Please enter your name:")
+# -----initialize the turtles-----
+t = trtl.Turtle()
+t.shape(t_shape)
+t.shapesize(t_size)
+t.fillcolor(t_color)
 
-score_writer.speed(0)
-score_writer.penup()
-score_writer.goto(-250, 250)
+score_writer = trtl.Turtle()
 score_writer.hideturtle()
+score_writer.penup()
+score_writer.goto(160, 160)  # x,y set to fit on smaller screen
+score_writer.pendown()
+# score_writer.showturtle()
 
-counter.speed()
-counter.penup()
-counter.goto(-300, 350)
+counter = trtl.Turtle()
 counter.hideturtle()
+counter.penup()
+counter.goto(-160, 160)  # x,y set to fit on smaller screen
+counter.pendown()
 
 
-t.speed(0)
-t.penup()
-t.shape("circle")
+# counter.showturtle()
 
+# -----game functions-----
 
-# -----game functions--------
-def manage_leaderboard():
-    global leader_scores_list
-    global leader_names_list
-    global score
-    global t
-
-    # load all the leaderboard records into the lists
-    lb.load_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list)
-
-    # TODO
-    if (len(leader_scores_list) < 5 or score > leader_scores_list[4]):
-        lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
-        lb.draw_leaderboard(leader_names_list, leader_scores_list, True, t, score)
-
-    else:
-        lb.draw_leaderboard(leader_names_list, leader_scores_list, False, t, score)
-
-
-def t_clicked(x, y):
-    update_score(x, y)
-    change_position()
-    colorsize()
-
-
-def change_position():
-    new_xpos = rand.randint(-350, 350)
-    new_ypos = rand.randint(-300, 300)
-    t.goto(new_xpos, new_ypos)
-
-
-def update_score(x, y):
-    global score
-    score += 1
-    score_writer.clear()
-    score_writer.write(score, font=font_setup)
-
-
+# countdown function
 def countdown():
     global timer, timer_up
     counter.clear()
@@ -91,19 +60,78 @@ def countdown():
         timer -= 1
         counter.getscreen().ontimer(countdown, counter_interval)
 
-
-def colorsize():
-    t.shapesize(rand.choice(t_size))
-    t.color(rand.choice(t_color))
+    # update and display the score
 
 
-# -----events----------------
-t.onclick(t_clicked)
+def update_score():
+    global score
+    score = score + 1
+    score_writer.clear()
+    score_writer.write(score, font=font_setup)
+
+
+# what happens when the spot is clicked
+def t_clicked(x, y):
+    global timer_up
+    if (not timer_up):
+        update_score()
+        change_position()
+    else:
+        t.hideturtle()
+
+
+# resize the turtle
+def resize():
+    sizes = [.5, 1, 1.5, 2]
+    t.shapesize(rand.choice(sizes))
+
+
+# stamp turtle
+def leave_a_mark():
+    t.fillcolor(rand.choice(colors[1:]))
+    t.stamp()
+    t.fillcolor(colors[0])  # comment out for more a more difficult game
+
+
+# change the position of spot
+def change_position():
+    leave_a_mark()  # challenge to add color
+    resize()  # challenge to change size of turtle
+    new_xpos = rand.randint(-150, 150)  # x,y set to fit on smaller screen
+    new_ypos = rand.randint(-150, 150)  # x,y set to fit on smaller screen
+    t.penup()  # 2nd step in moving
+    t.hideturtle()  # 3rd step in moving
+    t.goto(new_xpos, new_ypos)  # 1st step in moving
+    t.showturtle()
+    t.pendown()
+
+
+# manages the leaderboard for top 5 scorers
+def manage_leaderboard():
+    global score
+    global t
+
+    # get the names and scores from the leaderboard file
+    leader_names_list = lb.get_names(leaderboard_file_name)
+    leader_scores_list = lb.get_scores(leaderboard_file_name)
+
+    # show the leaderboard with or without the current player
+    if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+        lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+        lb.draw_leaderboard(True, leader_names_list, leader_scores_list, t, score)
+
+    else:
+        lb.draw_leaderboard(False, leader_names_list, leader_scores_list, t, score)
+
+
+# starting the game
+def start_game():
+    t.onclick(t_clicked)
+    counter.getscreen().ontimer(countdown, counter_interval)
+
+
+# ----------events----------
+start_game()
 wn = trtl.Screen()
-wn.bgcolor("white")
-wn.ontimer(countdown, counter_interval)
+wn.bgcolor("white smoke")
 wn.mainloop()
-
-# ------high scores-------------
-# 32
-
