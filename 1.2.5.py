dd9fcf6dab1b0ff5b71c9d32b215f6304fcc806a
SantiagocDaSilva <santiago.dasilva@vtsd.com>
2024-01-09 07:58:20 -0500

    1.2.5

---
+++ 1.2.5.py
@@ -0,0 +1,12 @@
+# Santiago DaSilva 1.2.5
+
+import turtle as trtl
+
+# draw tictactoe board
+ws = trtl.Screen()
+t = trtl.Turtle()
+
+board = ['|', '|', '|', '|', '|', '|', '|', '|', '|', '|']
+print(board[0:3])
+print(board[4:7])
+print(board[7:10])
---
+++ Parameters/Example1.py
@@ -0,0 +1,11 @@
+#Write a function called add_vals() that adds
+# Write the function so that if num1 is bigger than num2
+# it displays num3, otherwise it says invalid input
+
+def add_vals(num1, num2, num3):
+    if num1 > num2:
+        print(num3)
+    else:
+        print("Invalid input.")
+
+add_vals(3, 4, 7)
