d5ec1fa3a4851a901db03221c29f020bba25725b
SantiagocDaSilva <santiago.dasilva@vtsd.com>
2024-01-11 09:24:37 -0500

    1.2.5 1/11/2024

--- 1.2.5.py
+++ 1.2.5.py
@@ -3,10 +3,34 @@
 import turtle as trtl
 
 # draw tictactoe board
-ws = trtl.Screen()
-t = trtl.Turtle()
 
-board = ['|', '|', '|', '|', '|', '|', '|', '|', '|', '|']
-print(board[0:3])
-print(board[4:7])
-print(board[7:10])
+
+spaces = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+def drawBoard():
+    print(str(board[0:3]) + '\n')
+    print(str(board[3:6]) + '\n')
+    print(str(board[6:9]) + '\n')
+
+
+gameOver = False
+turns = 1
+drawBoard()
+
+while (turns<10):
+    turns += 1
+    move1 = int(input("Make a move, pick any of the numbers 1-9"))
+    board[move1-1]="X"
+    spaces.remove(move1-1)
+    drawBoard()
+    gameOver = False
+turns = 2
+drawBoard()
+while (turns<10):
+    turns += 2
+    move1=int(input("My turn"))
+    board[move1 - 2] = "O"
+    spaces.remove(move1 - 1)
+    drawBoard()
